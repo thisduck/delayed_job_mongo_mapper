62def0501984eed18621f42995d34fb1a931e16f
Adnan Ali <adnan.ali@gmail.com>
2012-02-23 23:59:58 -0500

    passing tests for syck parser.

--- lib/delayed/serialization/mongo_mapper.rb
+++ lib/delayed/serialization/mongo_mapper.rb
@@ -1,18 +1,21 @@
 # encoding: utf-8
 if YAML.parser.class.name =~ /syck/i
   MongoMapper::Document.class_eval do
+    @attributes_delayed = nil
     yaml_as "tag:ruby.yaml.org,2002:MongoMapper"
 
     def self.yaml_new(klass, tag, val)
+      val['attributes'] = val['attributes_delayed']
       begin
-        klass.find(val['attributes']['_id'])
+        klass.find!(val['attributes']['_id'])
       rescue MongoMapper::DocumentNotFound
         raise Delayed::DeserializationError
       end
     end
 
     def to_yaml_properties
-      ['@attributes']
+      @attributes_delayed = self.attributes
+      ['@attributes_delayed']
     end
   end
 else
